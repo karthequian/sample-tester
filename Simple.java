888a516c1efc3a0612c8f49cde6ef2fa3c4c6355
Karthik Gaekwad <karthequian@users.noreply.github.com>
2023-10-27 14:50:57 -0500

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 public class Simple {
     public static void main(String args[]) {
         System.out.println ("hello javapoint");
-        System.out.println("hello javatpoint");
+        System.out.println ("hello javatpoint");
     }
 }
